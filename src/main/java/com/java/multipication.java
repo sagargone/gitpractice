14294da37821b10df8efdee7b57b211f9762a3ad
sagargone <sagarneosoftqa@gmail.com>
2021-10-01 11:46:52 +0530

    Create multipication.java

---
+++ src/main/java/com/java/multipication.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class multipication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("multiplication");
+	}
+
+}
